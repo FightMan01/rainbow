cf96ef9ab6e9c178b897fe11ca4cd226539d9eef
FightMan01 <04aron@gmail.com>
2019-11-18 14:29:32 +0100

    Update rainbow.py

--- rainbow.py
+++ rainbow.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 from discord.utils import find
+import datetime
 
 client = commands.Bot(command_prefix=".")
 Client = discord.Client()
@@ -14,29 +15,37 @@ client.remove_command('help')
 
 @client.event
 async def on_ready():
+    jatek = discord.Game("Rainbow bot By.: FightMan01")
+    await client.change_presence(activity=jatek)
     try:
-        server2 = client.get_server(id="szerver id")
-        szszam = int(len(client.servers))
+        #Adatok bekérése
+        rangid = "rang id-ja"
+        szerverid = "szerver id-ja"
+        #Szerver lekérése
+        server2 = client.get_guild(id=int(szerverid))
         while True:
-            for server in client.servers:
-                role = discord.utils.get(server2.roles, id="rang id")
-                try:
-                    i = random.randint(0,1)
-                    colours = random.randint(0, 0xFFFFFF)
-                    await client.edit_role(server=server2, role=role, colour=discord.Colour(colours))
-                    await asyncio.sleep(5)
-                    await client.change_presence(game=discord.Game(name="Rainbow bot By.: FightMan01"))
-                except Exception as e:
-                    print(e)
-                    await client.change_presence(game=discord.Game(name="Rainbow bot By.: FightMan01"))
-                    await asyncio.sleep(5)
-                    continue
+            #Rang lekérése
+            role = discord.utils.get(server2.roles, id=int(rangid))
+            try:
+                #Rang frissítő rendszer
+                colours = random.randint(0, 0xFFFFFF)
+                await role.edit(colour=discord.Colour(colours), reason="Rainbow bot by: FightMan01")
+                #5 másodperc várakozás. Lehet nagyobbra is rakni, de kisebbre nem érdemes, mert a bot API bant kaphat.
+                await asyncio.sleep(5)
+                #Rendszer vége, ez alatt a hiba kezelő rendszer van.
+            except Exception as e:
+                #Hiba kiiratása
+                print(f"Hiba történt: {e}")
+                #Itt is 5 másodperc várakozás, hogy hiba esetén se ugorjon egyből vissza, mert API bant kaphat a bot.
+                await asyncio.sleep(5)
+                #Egyszerűen csak folytatódik a rendszer.
+                continue
     except:
         pass
 
-@client.command(pass_context=True)
+@client.command()
 async def help(ctx):
-    embed = discord.Embed(title="Rainbow bot".format(ctx.message.server.name), description="Készítette: FightMan01", color=0x00ff00, timestamp=ctx.message.timestamp)
-    await client.say(embed=embed)
+    embed = discord.Embed(title="Rainbow bot", description="Készítette: FightMan01", color=0x00ff00, timestamp=datetime.datetime.utcnow())
+    await ctx.send(embed=embed)
 
 client.run("TOKEN")
